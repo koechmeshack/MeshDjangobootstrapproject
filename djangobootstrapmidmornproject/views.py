70c20125637ff45b145d920e9ff572c89e6dfa10
koechmeshack <teddymesh196@gmail.com>
2023-02-24 13:10:02 +0300

    initial commit

---
+++ djangobootstrapmidmornproject/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+def index(request):
+    return render(request,'index.html')
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def fruit(request):
+    return render(request,'fruit.html')
+def testimonial(request):
+    return render(request,'testimonial.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
